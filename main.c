d9d0ac55d34ce60c2278a95295f49396311fd3bd
Omar Ben Brahim <om3rbb@gmail.com>
2026-03-28 13:24:26 +0100

    initialize getline buffer and add debug print

--- main.c
+++ main.c
@@ -7,8 +7,8 @@
 #include <stdbool.h>
 
 int main(void) {
-    char *buffer_inp;
-    size_t size_bu;
+    char *buffer_inp = NULL;
+    size_t size_bu = 0;
     while (1) {
         printf("msh $ ");
         ssize_t len = getline(&buffer_inp, &size_bu, stdin);
@@ -16,8 +16,9 @@ int main(void) {
             break;
         }
         printf("command received: %s", buffer_inp);
-        printf("%c", buffer_inp[strlen(buffer_inp) - 1]);
+        printf("-> %zu | %zd%c", size_bu, len, buffer_inp[strlen(buffer_inp) - 1]);
     }
 
+    free(buffer_inp);
     return 0;
 }
