703db5a1d8dd979608d3a0346707f4132bbc5bf3
Omar Ben Brahim <om3rbb@gmail.com>
2026-03-28 14:36:37 +0100

    add parse_command and switch to fgets input

--- main.c
+++ main.c
@@ -5,20 +5,60 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
+
+#define BUFFER_MAX_SIZE 1024
+#define COMM_AV 3 
+
+void parse_command(char *command) {
+    char *internal_commands[COMM_AV] = {"cd", "exit", "help"};
+    char **args = NULL;
+    int count_args = 0;
+    char *token;
+    char *t_command = malloc(strlen(command) + 1);
+    strcpy(t_command, command);
+    token = strtok(t_command, " ");
+    while (token != NULL) {
+        args = realloc(args, sizeof(char *) * (count_args + 1));
+        if (!args) {
+            printf("realloc failed!");
+            exit(1);
+        }
+        args[count_args] = strdup(token);
+        if (!args[count_args]) {
+            printf("realloc failed!");
+            exit(1);
+        }
+        count_args++;
+        token = strtok(NULL, " ");
+    }
+    args = realloc(args, sizeof(char *) * (count_args + 1));
+    args[count_args] = NULL;
+    for (size_t i = 0; i < COMM_AV; i++) {
+        if (strcmp(args[0], internal_commands[i]) == 0) {
+            system(command);
+        }
+    }
+    // free resources
+    for (size_t i = 0; i < count_args; i++) free(args[i]);
+    free(args);
+    free(t_command);
+}
 
 int main(void) {
-    char *buffer_inp = NULL;
-    size_t size_bu = 0;
+    char buffer_inp[BUFFER_MAX_SIZE];
     while (1) {
         printf("msh $ ");
-        ssize_t len = getline(&buffer_inp, &size_bu, stdin);
-        if (len == -1) {
-            break;
+        fflush(stdout);
+        // use getline more convenient for dynamic buffers!
+        if (fgets(buffer_inp, BUFFER_MAX_SIZE, stdin)) {
+            buffer_inp[strcspn(buffer_inp, "\n")] = '\0';
+            // printf("command received: %s\n", buffer_inp);
+            parse_command(buffer_inp);
+            // printf("%s", buffer_inp);
+        } else {
+            printf("command failed!");
         }
-        printf("command received: %s", buffer_inp);
-        printf("-> %zu | %zd%c", size_bu, len, buffer_inp[strlen(buffer_inp) - 1]);
     }
-
-    free(buffer_inp);
     return 0;
 }
